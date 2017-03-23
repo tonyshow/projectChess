fd3a320fa9537e0b78a20de31204b3fd6cfc3164
tonyshow <994262676@qq.com>
2017-03-23 22:32:09 +0800

    新增麻将表

---
+++ shared/Consts.js
@@ -0,0 +1,10 @@
+module.exports = {
+    
+	//座位编号 安装逆时针
+	SEATNO:{
+		Frist	:	1,	//首次进入房间的人
+		Second	:	2,
+		Third	:	3,
+		Fourth	:	4
+	}
+}
---
+++ shared/code.js
@@ -0,0 +1,6 @@
+module.exports = {
+    OK: 200,
+    FAIL: 500,
+    // 数据库错误
+    DB_ERROR: 501,
+};
