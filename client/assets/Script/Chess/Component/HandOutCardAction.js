7c842a72df23e1d4557a2393fb409b8eaaacb26e
tony <994262676@qq.com>
2017-03-24 09:25:23 +0800

    手牌动作

---
+++ client/assets/Script/Chess/Component/HandOutCardAction.js
@@ -0,0 +1,17 @@
+/**
+ * 手牌出牌动作
+ * author:tony
+ * time : 2017/03/24
+ */
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+       
+    },
+
+    /***touch */
+    doTouch:function(){
+
+    }   
+});
---
+++ client/assets/Script/Chess/Component/HandState.js
@@ -0,0 +1,14 @@
+/**
+ * 手牌状态
+ * author:tony
+ * time : 2017/03/24
+ */
+
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+      
+    }, 
+});
