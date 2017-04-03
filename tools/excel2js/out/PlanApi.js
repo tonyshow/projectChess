c7db8013f34c92cf9cef6b3b5e59c89b710b116c
tonyshow <994262676@qq.com>
2017-04-03 14:24:23 +0800

    新增扑克表

--- tools/excel2js/out/PlanApi.js
+++ tools/excel2js/out/PlanApi.js
@@ -5,12 +5,16 @@ var exp = module.exports = {};
 
 var modules = exp.modules = {};
 
-var names =['PlanCommonParameter', 'PlanMjConfig'];
+var names =['PlanCommonParameter', 'PlanPkConfig', 'PlanMjConfig'];
 
 function create( name ){
 	if("PlanCommonParameter"==name){
 		var PlanCommonParameter = require('./../../MgData/PlanData/PlanCommonParameter');
 		return new PlanCommonParameter();
+	}
+	else if("PlanPkConfig"==name){
+		var PlanPkConfig = require('./../../MgData/PlanData/PlanPkConfig');
+		return new PlanPkConfig();
 	}
 	else if("PlanMjConfig"==name){
 		var PlanMjConfig = require('./../../MgData/PlanData/PlanMjConfig');
@@ -18,7 +22,7 @@ function create( name ){
 	}
 }
 function doman(){   
-    for(var i = 0 ; i < 2 ; ++i ){
+    for(var i = 0 ; i < 3 ; ++i ){
         var name = names[i];
         Object.defineProperty(exp, name, {
             get: (function (name) {
